ecd7ef11ea4ce4dd0eaf96a0446a6e866ff8771e
Евгений Фуреев <efureev@yandex.ru>
2018-12-03 13:58:35 +0500

    fix

--- src/NestedSetServiceProvider.php
+++ src/NestedSetServiceProvider.php
@@ -5,15 +5,24 @@ namespace Fureev\Trees;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\ServiceProvider;
 
+/**
+ * Class NestedSetServiceProvider
+ *
+ * @package Fureev\Trees
+ */
 class NestedSetServiceProvider extends ServiceProvider
 {
+    /**
+     *
+     */
     public function register()
     {
-        Blueprint::macro('installNestedSet', function () {
-            NestedSetConfig::getColumns($this);
+        Blueprint::macro('installNestedSet', function ($instance) {
+            NestedSetConfig::getColumns($instance);
         });
-        Blueprint::macro('dropNestedSet', function () {
-            NestedSetConfig::dropColumns($this);
+
+        Blueprint::macro('dropNestedSet', function ($instance) {
+            NestedSetConfig::dropColumns($instance);
         });
     }
 }
